3e1204bbea3993dd97fcb0216533d476993f879a
Rodolfo Mayora Pereda <rodolfomayora96@gmail.com>
2021-06-07 10:58:20 -0400

    RT14: QuantityFilters component

--- src/components/QuantityFilter/index.tsx
+++ src/components/QuantityFilter/index.tsx
@@ -1,48 +1,33 @@
-import { FC, useState, useContext } from 'react';
-//import style from './style.module.scss';
-
+import { FC, useContext } from 'react';
 import { filterStateContext, filterSetContext } from '../../context/filters';
+import CustomSelectInput from '../CustomSelectInput'
 
 const QuantityFilter: FC = () => {
 
-  const { currentQuantityItems, quantityOptions } = useContext(filterStateContext);
+  const { currentQuantityItems } = useContext(filterStateContext);
   const setFilters = useContext(filterSetContext);
-
-  const [showOptions, setShowOptions] = useState<boolean>(false);
   
-  const invertBoolean = (state: boolean): boolean => !state;
+  const label: string = 'Items to show';
+  const options: object = {
+    '25': 25,
+    '50': 50,
+    '100': 100
+  }
 
-  const selectOption = (option: number) => {
+  const selectOption = (option: number): void => {
     setFilters((state: any) => ({
       ...state,
       currentQuantityItems: option
     }));
-
-    setShowOptions(invertBoolean);
   }
 
-  const onClickToggle = () => setShowOptions(invertBoolean);
-
   return (
-    <div>
-      <div onClick={onClickToggle}>
-        show: {currentQuantityItems}
-      </div>
-
-      {showOptions && (
-        <ul>
-          {!!quantityOptions.length && quantityOptions.map((option: number) => (
-            <li
-              key={option.toString()}
-              onClick={() => selectOption(option)}
-            >
-              {option}
-            </li>
-          ))}
-        </ul>
-      )}
-      
-    </div>
+    <CustomSelectInput
+      label={label}
+      options={options}     
+      currentOption={currentQuantityItems}
+      setCurrentOption={selectOption}
+    />
   )
 }
 
